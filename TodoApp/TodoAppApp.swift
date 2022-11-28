3e51274cc36da375dbb77735d10757dc6d87c3b8
LeThachLam <lamlt@mobiustech.dev>
2022-11-28 23:39:35 +0700

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Thạch Lâm Lê on 28/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
