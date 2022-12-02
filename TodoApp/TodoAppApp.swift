c176d8a704da0395e8751ea5e1836514206d5448
LeThachLam <lamlt@mobiustech.dev>
2022-12-02 22:25:19 +0700

    Add auth

--- TodoApp/TodoAppApp.swift
+++ TodoApp/TodoAppApp.swift
@@ -6,14 +6,35 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct TodoAppApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject private var todos = TodoController(todos: fakeData)
     var body: some Scene {
         WindowGroup {
-            MainScreen()
-                .environmentObject(todos)
+            TabView{
+                MainScreen()
+                    .environmentObject(todos)
+                    .tabItem {
+                        Label("Home", systemImage: "homekit")
+                    }
+                AuthScreen()
+                    .tabItem {
+                        Label("Setting", systemImage: "gear")
+                    }
+
+            }
+            
         }
     }
 }
---
+++ TodoApp/Views/AuthScreen.swift
@@ -0,0 +1,20 @@
+//
+//  AuthScreen.swift
+//  TodoApp
+//
+//  Created by Thạch Lâm Lê on 01/12/2022.
+//
+
+import SwiftUI
+
+struct AuthScreen: View {
+    var body: some View {
+        Text("Authentication Page")
+    }
+}
+
+struct AuthScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthScreen()
+    }
+}
